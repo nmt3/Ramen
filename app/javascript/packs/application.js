642a7503774540e176b0e938e3c2cc251204892f
EC2 Default User <ec2-user@ip-172-31-9-189.ap-northeast-1.compute.internal>
2023-01-19 11:33:53 +0000

    change layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -25,5 +25,5 @@ window.raty = function(elem, opt) {
 // require('packs/raty')
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
