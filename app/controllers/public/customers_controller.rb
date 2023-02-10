16de22e7bb54271a25d7d7c9c2d811523565aa78
EC2 Default User <ec2-user@ip-172-31-9-189.ap-northeast-1.compute.internal>
2023-02-10 14:09:39 +0000

    layout & partial template

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,7 +3,7 @@ class Public::CustomersController < ApplicationController
 
   def show
     @customer = Customer.find(params[:id])
-    @posts = current_customer.posts
+    @posts = current_customer.posts.page(params[:page]).per(10)
   end
 
   def list
